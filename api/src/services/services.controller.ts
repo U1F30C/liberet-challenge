6bdc7188070771c4fd65ae84c54438cc6845b843
Eric <eric.lopezs@alumnos.udg.mx>
2021-09-19 03:21:39 -0500

    Add service operations endpoints

--- api/src/services/services.controller.ts
+++ api/src/services/services.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post } from "@nestjs/common";
+import { ServicesService } from "./services.service";
 
-@Controller('services')
-export class ServicesController {}
+@Controller("services")
+export class ServicesController {
+  constructor(private servicesService: ServicesService) {}
+  @Get()
+  getAll() {
+    return this.servicesService.getAll();
+  }
+
+  @Post(":serviceId(\\d+)/consume/:userId(\\d+)")
+  useService(
+    @Param("serviceId") serviceId: string,
+    @Param("userId") userId: string
+  ) {
+    return this.servicesService.consumeService(userId, serviceId);
+  }
+
+  @Post(":serviceId(\\d+)/start/:userId(\\d+)")
+  startService(
+    @Param("serviceId") serviceId: string,
+    @Param("userId") userId: string
+  ) {
+    return this.servicesService.startService(userId, serviceId);
+  }
+
+  @Post(":serviceId(\\d+)/stop/:userId(\\d+)")
+  stopService(
+    @Param("serviceId") serviceId: string,
+    @Param("userId") userId: string
+  ) {
+    return this.servicesService.stopService(userId, serviceId);
+  }
+}
