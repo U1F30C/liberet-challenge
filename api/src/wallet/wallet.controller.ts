af46f3531bf64a9a4e3461b64abdce8a07d8c600
Eric <eric.lopezs@alumnos.udg.mx>
2021-09-19 03:59:17 -0500

    Add wallet recharge endpoint

--- api/src/wallet/wallet.controller.ts
+++ api/src/wallet/wallet.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import { Body, Controller, Get, Param } from "@nestjs/common";
 import { WalletService } from "./wallet.service";
 
 @Controller("wallet")
@@ -7,5 +7,12 @@ export class WalletController {
   @Get(":userId(\\d+)")
   getWallet(@Param("userId") userId: string) {
     return this.walletService.getOrCreateWallet(userId);
+  }
+  @Get(":userId(\\d+)/recharge")
+  rechargeWallet(
+    @Param("userId") userId: string,
+    @Body("amount") amount: string
+  ) {
+    return this.walletService.recharge(userId, amount);
   }
 }
